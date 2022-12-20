29b2b261b546bb570bcaf5198dc664c892f6f09e
김한울 <gimhan-ul@gimhan-ul-ui-MacBookAir.local>
2022-12-21 08:35:41 +0900

    edit-design

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container").children;
 const 이전 = document.querySelector(".이전");
 const 다음 = document.querySelector(".다음");
 const q5_input = container[4].querySelectorAll("input");
+const q9_input = container[8].querySelector("input");
 i = 0;
 function getindex(params) {
     if (localStorage.getItem("index") > 0) {
@@ -11,7 +12,7 @@ function getindex(params) {
     }
 }
 function blockClick() {
-    if (i == 4) {
+    if (i == 4 || i == 8) {
         btnDisabled();
     }else{
         btnActive();
@@ -44,6 +45,17 @@ function btnActive()  {
     const target = document.getElementById('target_btn');
     target.disabled = true;
   }
+  function checkQ9() {
+    if (q9_input.value == 2002) {
+        container[i].classList.add("inv");
+        i += 1;
+        container[i].classList.remove("inv");
+        localStorage.setItem("index", i);
+        btnActive();
+    }else{
+        alert("다시 생각해봐!");
+    }
+  }
   function checkQ5() {
     if (q5_input[0].value == 95 && q5_input[1].value == 12 && q5_input[2].value == 2 && q5_input[3].value == 5) {
         container[i].classList.add("inv");
@@ -56,5 +68,6 @@ function btnActive()  {
     }
   }
   getindex();
+blockClick();
 다음.addEventListener("click", rightBtn);
 이전.addEventListener("click", leftBtn)
