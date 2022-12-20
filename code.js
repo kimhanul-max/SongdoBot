b6165f511d5b9bf1f51f616f0f1a01ed7e368871
김한울 <gimhan-ul@gimhan-ul-ui-MacBookAir.local>
2022-12-20 16:15:22 +0900

    8page

--- code.js
+++ code.js
@@ -1,21 +1,60 @@
 const container = document.querySelector(".container").children;
 const 이전 = document.querySelector(".이전");
 const 다음 = document.querySelector(".다음");
-
+const q5_input = container[4].querySelectorAll("input");
 i = 0;
+function getindex(params) {
+    if (localStorage.getItem("index") > 0) {
+        container[i].classList.add("inv");
+        i += parseInt(localStorage.getItem("index"));
+        container[i].classList.remove("inv");
+    }
+}
+function blockClick() {
+    if (i == 4) {
+        btnDisabled();
+    }else{
+        btnActive();
+    }
+}
 function rightBtn() {
     if (i < container.length - 1) {
         container[i].classList.add("inv");
         i += 1;
+        localStorage.setItem("index", i);
         container[i].classList.remove("inv");
     }
+    blockClick()
 }
 function leftBtn() {
     if (i > 0) {
         container[i].classList.add("inv");
         i -= 1;
+        localStorage.setItem("index", i);
         container[i].classList.remove("inv");
     }
+    blockClick()
 }
+function btnActive()  {
+    const target = document.getElementById('target_btn');
+    target.disabled = false;
+  }
+  
+  function btnDisabled()  {
+    const target = document.getElementById('target_btn');
+    target.disabled = true;
+  }
+  function checkQ5() {
+    if (q5_input[0].value == 95 && q5_input[1].value == 12 && q5_input[2].value == 2 && q5_input[3].value == 5) {
+        container[i].classList.add("inv");
+        i += 1;
+        container[i].classList.remove("inv");
+        localStorage.setItem("index", i);
+        btnActive();
+    }else{
+        alert("다시 생각해봐!");
+    }
+  }
+  getindex();
 다음.addEventListener("click", rightBtn);
 이전.addEventListener("click", leftBtn)
